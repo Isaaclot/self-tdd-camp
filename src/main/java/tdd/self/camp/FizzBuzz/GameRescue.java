48560752b355164ada91e0184aa15f4699683e4e
梁仟 <liangqian0723@gmail.com>
2019-09-05 14:32:13 +0800

    7d rescue fizzbuzz

---
+++ src/main/java/tdd/self/camp/FizzBuzz/GameRescue.java
@@ -0,0 +1,30 @@
+package tdd.self.camp.FizzBuzz;
+
+/**
+ * @author alis
+ * @date 2019/9/5 2:06 PM
+ * @description
+ */
+public class GameRescue {
+
+    public static final String FIZZ = "fizz";
+    public static final String BUZZ = "buzz";
+
+    public static String say(int i) {
+        if (isDividable(i, 3) && isDividable(i, 5)) {
+            return FIZZ + BUZZ;
+        }
+        if (isDividable(i, 3)) {
+            return FIZZ;
+        }
+        if (isDividable(i, 5)) {
+            return BUZZ;
+        }
+
+        return i + "";
+    }
+
+    private static boolean isDividable(int i, int i1) {
+        return i % i1 == 0;
+    }
+}
